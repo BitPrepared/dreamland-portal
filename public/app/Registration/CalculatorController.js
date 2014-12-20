eb241d06ea90cfed9b77551cd0513ed8e738cec5
Stefano Tamagnini <yoghi@sigmalab.net>
2014-12-20 03:43:49 +0100

    rimove "debugger"

--- public/app/Registration/CalculatorController.js
+++ public/app/Registration/CalculatorController.js
@@ -4,7 +4,6 @@ define(['angular','dreamApp'], function(angular,dreamApp){
   var calculator = dreamApp.controller('CalculatorController', function ($scope, $rootScope, $http, $state, $stateParams) {
 
     $scope.update = function() {
-      debugger;
       $scope.parent.punteggiosquadriglia = $scope.parent.numerosquadriglieri + $scope.parent.specialitasquadriglieri + (4 * $scope.parent.brevettisquadriglieri);
       if ( $scope.parent.specialitadisquadriglia ) {
         $scope.parent.punteggiosquadriglia += 10;
