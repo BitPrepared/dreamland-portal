681dc6a471d68da0f53ad64e3d83790e1a814496
Stefano Tamagnini <yoghi@sigmalab.net>
2015-01-24 22:54:07 +0100

    fix from and to address format

--- src/BitPrepared/Mail/MailgunSender.php
+++ src/BitPrepared/Mail/MailgunSender.php
@@ -14,12 +14,25 @@ class MailgunSender
 
     public function __construct($logger,$from,$mailgunConfig){
         $this->log = $logger;
-        $this->from = $from;
+        $fromKeys = array_keys($from);
+        $fromvalues = array_values($from);
+        $this->from = '\''.$fromvalues[0].' <'.$fromKeys[0].'>\'';
         $this->pubKey = $mailgunConfig['pubkey'];
         $this->domain = $mailgunConfig['domain'];
         $this->apikey = $mailgunConfig['key'];
     }
 
+    /**
+     * @param $toEmailAddress 1 sola mail di destinazione mail => nome destinatario
+     * @param $subject
+     * @param $txtMessage
+     * @param null $htmlMessage
+     * @param null $attachment
+     * @return bool
+     *
+     * FIXME: supporto multi TO address e CC address
+     *
+     */
     public function send($toEmailAddress, $subject, $txtMessage, $htmlMessage = null, $attachment = null){
 
         $mgClient = new Mailgun($this->apikey);
@@ -39,6 +52,12 @@ class MailgunSender
 //        );
 
         try {
+
+            $fromKeys = array_keys($toEmailAddress);
+            $fromvalues = array_values($toEmailAddress);
+            $toEmailAddress = '\''.$fromvalues[0].' <'.$fromKeys[0].'>\'';
+
+            $this->log->info('From mail : '.$this->from.' => TO => '.$toEmailAddress);
 
             $result = $mgClient->sendMessage($this->domain,
                 array(
