c5e64955b8d2e75fed1667f490ab019d6aa28ac6
Stefano Tamagnini <yoghi@sigmalab.net>
2015-05-30 19:09:26 +0200

    update composer script selector

--- composer.sh
+++ composer.sh
@@ -2,12 +2,14 @@
 
 rm composer.lock
 
+echo "current php version $TRAVIS_PHP_VERSION"
+
 if [ "$TRAVIS_PHP_VERSION" = "5.3.29"  ]; then
     echo "5.3 detected"
     cp composer.5.3.json composer.json
 fi
 
-composer install --dev -n --prefer-source
+composer install -n --prefer-source
 
 
 
